bafddb2933b7b8deff6066038ca6ca9f688c193e
Son Of Fjord <v4lkyn@yandex.ru>
2023-02-23 16:52:55 +0300

    textures have been added

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import pygame as pg
 import sys
 from game.settings import *
-from game.map import * 
+from game.map import *
 from game.player import *
 from game.raycasting import *
+from game.object_renderer import *
 
 
 class Game:
     def __init__(self):
         pg.init()
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.new_game()
-    
+
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
 
     def update(self):
@@ -27,9 +30,9 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        self.screen.fill('black')
-        #self.map.draw()
-        #self.player.draw()
+        self.object_renderer.draw()
+        # self.map.draw()
+        # self.player.draw()
 
     def check_events(self):
         for event in pg.event.get():
